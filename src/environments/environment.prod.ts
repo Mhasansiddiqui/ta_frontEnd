373d8b9a27f5a72d32d281cbb2e720ace1d5ffc9
Mhasansiddiqui <hasancomsoft@gmail.com>
2025-10-23 22:20:59 +0500

    Updated Vercel configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-backend-api-url.com', // You'll need to update this with your deployed backend URL
+  freeCurrencyApiKey: process.env['FREECURRENCY_API_KEY']
+};
