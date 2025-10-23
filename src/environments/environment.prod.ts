3e621f0ed69da199847f20aef1033b75d535f2e7
Mhasansiddiqui <hasancomsoft@gmail.com>
2025-10-23 22:59:32 +0500

    Fixed TypeScript syntax in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://respectful-reflection-production.up.railway.app',
-  freeCurrencyApiKey: ''
-};
+  freeCurrencyApiKey: '' as string
+}
   freeCurrencyApiKey: process.env['FREECURRENCY_API_KEY']
 };
